641916f3176a7140d6c57be38cfb99b32b7b3387
denkeni <denkenie@gmail.com>
2025-09-29 10:30:45 +0800

    Add a few projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,67 +9,27 @@ export interface Project {
 }
 
 export const projectsData: Project[] = [{
-  id: 'tamagochi-at-riverside',
+  id: '44917272433563877177165448180993403912192-42220-0x16bA53B74c234C870c61EFC04cD418B8f2865959',
   name: 'Tamagochi at Riverside',
   image: 'https://app.hypercerts.org/_next/image?url=%2Fapi%2Fhypercerts%2F42220-0x16bA53B74c234C870c61EFC04cD418B8f2865959-44917272433563877177165448180993403912192%2Fimage&w=1080&q=75',
   org: '[Name of the org]',
-  timeline: '2024/10/09 → 2024/10/9',
-  description: 'FAB DAO、多名 Formosa Art Bank DAO、為公益 NFT 藝品展覽、集結跨領域藝術家、嫁接臺灣 NFT 藝品公益、藝術、文化的新意義..',
+  timeline: '2023-03-01 → 2023-12-31',
+  description: 'Beyond the school, the project reached over 50,000 local residents and connected with 20+ online and offline communities through workshops, digital campaigns, and habitat restoration events.',
   tags: ['FABDAO', 'ART']
 }, {
-  id: 'fab-dao-hc-kickoff',
+  id: '159932712452841077827786065492931059384320-10-0x822F17A9A5EeCFd66dBAFf7946a8071C265D1d07',
   name: 'FAB DAO | HC_KickOff',
   image: 'https://app.hypercerts.org/_next/image?url=%2Fapi%2Fhypercerts%2F10-0x822F17A9A5EeCFd66dBAFf7946a8071C265D1d07-159932712452841077827786065492931059384320%2Fimage&w=1080&q=75',
-  org: '超證實驗室',
-  timeline: 'Feb 20, 2023 — Mar 31, 2023',
-  description: '協助填寫 FAB DAO 超證申請書、研究與制定發行方式，並透過社群 AMA 達成共識。We established the FAB DAO HyperCert application form, formulated the mechanism and published it to our community through an AMA. FAB DAO 專注於非營利NFT鑄造鑄造與第三部門區塊鏈入口。集結組織與個人，打造公益、藝術、文化之Web3跨域社群。FAB DAO commits to the improvement of public good through Web3 ecosystems. Banner Image: Project % by artist Che-Yu Wu',
+  org: 'Hypercerts Lab',
+  timeline: '2023-03-01 → 2023-12-31',
+  description: 'We established the FAB DAO HyperCert application form, formulated the mechanism and published it to our community through an AMA. FAB DAO commits to the improvement of public good through Web3 ecosystems. Banner Image: Project % by artist Che-Yu Wu.',
   tags: ['FABDAO', 'ART']
 }, {
-  id: 'tamagochi-at-riverside-2',
-  name: 'Tamagochi at Riverside',
-  image: 'https://app.hypercerts.org/_next/image?url=%2Fapi%2Fhypercerts%2F42220-0x16bA53B74c234C870c61EFC04cD418B8f2865959-44917272433563877177165448180993403912192%2Fimage&w=1080&q=75',
+  id: '18420165086164240904199394249496476822536192-10-0x822F17A9A5EeCFd66dBAFf7946a8071C265D1d07',
+  name: 'Shoutout X FABDAO | 來一 shot',
+  image: 'https://app.hypercerts.org/_next/image?url=%2Fapi%2Fhypercerts%2F10-0x822F17A9A5EeCFd66dBAFf7946a8071C265D1d07-18420165086164240904199394249496476822536192%2Fimage&w=1080&q=75',
   org: '[Name of the org]',
-  timeline: '2024/10/09 → 2024/10/9',
-  description: 'This is a sample project description for project 5.',
-  tags: ['FABDAO', 'ART']
-}, {
-  id: 'tamagochi-at-riverside-3',
-  name: 'Tamagochi at Riverside',
-  image: 'https://images.unsplash.com/photo-1629392554711-1b3a44375322?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80',
-  org: '[Name of the org]',
-  timeline: '2024/10/09 → 2024/10/9',
-  description: 'This is a sample project description for project 6.',
-  tags: ['FABDAO', 'ART']
-}, {
-  id: 'tamagochi-at-riverside-4',
-  name: 'Tamagochi at Riverside',
-  image: 'https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80',
-  org: '[Name of the org]',
-  timeline: '2024/10/09 → 2024/10/9',
-  description: 'This is a sample project description for project 7.',
-  tags: ['FABDAO', 'ART']
-}, {
-  id: 'tamagochi-at-riverside-5',
-  name: 'Tamagochi at Riverside',
-  image: 'https://images.unsplash.com/photo-1518531933037-91b2f5f229cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2564&q=80',
-  org: '[Name of the org]',
-  timeline: '2024/10/09 → 2024/10/9',
-  description: 'This is a sample project description for project 8.',
-  tags: ['FABDAO', 'ART']
-}, {
-  id: 'tamagochi-at-riverside-6',
-  name: 'Tamagochi at Riverside',
-  image: 'https://images.unsplash.com/photo-1629392554711-1b3a44375322?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80',
-  org: '[Name of the org]',
-  timeline: '2024/10/09 → 2024/10/9',
-  description: 'This is a sample project description for project 9.',
-  tags: ['FABDAO', 'ART']
-}, {
-  id: 'tamagochi-at-riverside-7',
-  name: 'Tamagochi at Riverside',
-  image: 'https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80',
-  org: '[Name of the org]',
-  timeline: '2024/10/09 → 2024/10/9',
-  description: 'This is a sample project description for project 10.',
+  timeline: '2023-11-01 → 2023-12-31',
+  description: 'The megaphone "Shout 鼠", co-created by da0 and FAB DAO, officially launched on Discord on 2023/11/21. This marks the first cross-community collaboration between da0 and FAB DAO in expressing gratitude loudly. We will continue to offer various forms of Shoutout megaphones for different communities and use cases, documenting each significant moment and allowing Shoutout to become an achievement backpack that users carry with them.',
   tags: ['FABDAO', 'ART']
 }];
