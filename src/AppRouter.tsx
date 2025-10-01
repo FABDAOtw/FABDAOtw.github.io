ac6a9290238de9c4888b57053f60a8f94ca8734e
denkeni <denkenie@gmail.com>
2025-10-01 22:06:23 +0800

    Attempt to fix project detail and about page URLs

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { App } from './App';
 import { About } from './pages/About';
 import { ProjectDetail } from './pages/ProjectDetail';
 import ScrollToTop from './components/ScrollToTop';
 
 export function AppRouter() {
-  return <BrowserRouter>
+  return <HashRouter>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
         <Route path="/project/:id" element={<ProjectDetail />} />
       </Routes>
-    </BrowserRouter>;
+    </HashRouter>;
 }
